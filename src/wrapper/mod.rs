2ddf50e790c11a8bfebcb47d5689083abffe2404
Tine Zata <60703214+TineZata@users.noreply.github.com>
2025-01-27 19:16:05 +0000

    clean up

--- src/wrapper/mod.rs
+++ src/wrapper/mod.rs
@@ -26,11 +26,11 @@ pub fn get_version_str() -> String {
 /// ```rust
 /// let ctx: Context = pvxs::get_context_from_env();
 /// if ctx._private._base._ptr.is_null() {
-///    println!("Failed to create context from environment");
-///   return;
+///     println!("Failed to create context from environment");
+///     return;
+/// } else {
+///     ctx
 /// }
-/// println!("Context created from environment");
-/// dbg!(ctx);
 /// ```
 ///
 pub fn get_context_from_env() -> Context {
@@ -45,14 +45,18 @@ pub fn get_context_from_env() -> Context {
 /// let config: Config = pvxs::get_context_config();
 /// let addr = unsafe { config.address_list.to_rust_string() };
 /// println!("Address list: {}", addr);
+/// 
 /// let interfaces = unsafe { config.interfaces.to_rust_string() };
 /// println!("Interfaces: {}", interfaces);
+/// 
 /// let name_servers = unsafe { config.name_servers.to_rust_string() };
 /// println!("Name servers: {}", name_servers);
+/// 
 /// println!("UDP port: {}", config.udp_port);
 /// println!("TCP port: {}", config.tcp_port);
 /// println!("TCP timeout: {}", config.tcp_timeout);
 /// println!("Auto address list: {}", if config.auto_addr_list { "true" } else { "false" });
+/// 
 /// config
 /// ```
 /// 
